ee551ee3177271cfaf3d18b73e3986776ed099de
Santiago Velosa <santiagovelosa@gmail.com>
2022-01-27 18:53:24 -0500

    Implement functionality to render Missions in MyProfile page

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const MyProfile = () => (
-  <h1>My profile!</h1>
-);
+const MyProfile = () => {
+  const missions = useSelector((state) => state.missions.missions);
+
+  return (
+    <div>
+      <h3>My Missions</h3>
+      <ul className="list-group">
+        {missions.filter((mission) => mission.reserved).map((mission) => (
+          <li className="list-group-item" key={mission.id}>{mission.name}</li>
+        ))}
+      </ul>
+
+    </div>
+  );
+};
 
 export default MyProfile;
