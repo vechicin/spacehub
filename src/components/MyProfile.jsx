cfeab203196edde981a1d3fd589c15bead6c6ed6
Santiago Velosa <santiagovelosa@gmail.com>
2022-01-27 19:32:55 -0500

    Finish styling for MyProfile page

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -3,16 +3,28 @@ import { useSelector } from 'react-redux';
 
 const MyProfile = () => {
   const missions = useSelector((state) => state.missions.missions);
+  const rockets = useSelector((state) => state.rockets.rockets);
 
   return (
-    <div>
-      <h3>My Missions</h3>
-      <ul className="list-group">
-        {missions.filter((mission) => mission.reserved).map((mission) => (
-          <li className="list-group-item" key={mission.id}>{mission.name}</li>
-        ))}
-      </ul>
-
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h3>My Missions</h3>
+          <ul className="list-group">
+            {missions.filter((mission) => mission.reserved).map((mission) => (
+              <li className="list-group-item" key={mission.id}>{mission.name}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="col">
+          <h3>My Rockets</h3>
+          <ul className="list group">
+            {rockets.filter((rocket) => rocket.reserved).map((rocket) => (
+              <li className="list-group-item" key={rocket.id}>{rocket.name}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
